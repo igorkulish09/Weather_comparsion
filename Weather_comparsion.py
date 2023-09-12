a9a1b703aa9b3c6e3f693e22c27127a805704a53
igor.kulish <igor.kulish09@gmail.com>
2023-09-12 13:03:43 +0300

    First commit

---
+++ Weather_comparsion.py
@@ -0,0 +1,82 @@
+import aiohttp
+import asyncio
+import datetime
+import logging
+
+
+# Настройка логгера
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+async def fetch_weather_data_hgbrasil():
+    api_url = "https://api.hgbrasil.com/weather"
+    params = {
+        "key": "8710fd8f9d34a216bf42cb362b736263",
+        "woeid": 455825,
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url, params=params) as response:
+            if response.status != 200:
+                logger.error(f"Ошибка при запросе данных о погоде с сайта hgbrasil.com: {response.status}")
+                return None
+            data = await response.json()
+            logger.info("Успешно получены данные о погоде с сайта hgbrasil.com")
+            return data
+
+
+async def fetch_weather_data_weatherstack():
+    api_url = "https://api.weatherstack.com/current"
+    params = {
+        "access_key": "2578e7e6b23b5f9ae610cda221cf9ed1",
+        "query": "San Francisco, United States of America",
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url, params=params) as response:
+            data = await response.json()
+            return data
+
+async def fetch_weather_data_accuweather():
+    api_url = "https://dataservice.accuweather.com/currentconditions/v1/12345"  # Пример идентификатора местоположения
+    params = {
+        "apikey": "3818e9ceef2a4365ab1d728f283c51ed",
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(api_url, params=params) as response:
+            data = await response.json()
+            return data
+
+
+async def main():
+    logger.info('Запуск скрипта')
+    tasks = [
+        fetch_weather_data_hgbrasil(),
+        fetch_weather_data_weatherstack(),
+        fetch_weather_data_accuweather(),
+    ]
+
+    responses = await asyncio.gather(*tasks)
+
+    # Преобразование данных в единый формат и сравнение
+    temperatures = []
+    for data in responses:
+        temperature = None
+        if data and data.get("temperature"):
+            temperature = data["temperature"]
+        temperatures.append(temperature)
+
+    # Вычисление средней температуры
+    valid_temperatures = [temp for temp in temperatures if temp is not None]
+    if valid_temperatures:
+        average_temperature = sum(valid_temperatures) / len(valid_temperatures)
+        logger.info(f"Средняя температура: {average_temperature}°C")
+    else:
+        logger.warning("Нет доступных данных о температуре.")
+
+    logger.info('Завершение выполнения скрипта')
+
+if __name__ == "__main__":
+    start = datetime.datetime.now()
+    logger.info('Start')
+    asyncio.run(main())
+    logger.info(f"Done in {datetime.datetime.now() - start}")
